e1a2051049f9ba65e2d461577d2130637948d29c
zhangxuqiang <zhangxuqiang@qn.com>
2019-12-09 20:23:45 +0800

    修复一些bug

--- lib/menuOp.py
+++ lib/menuOp.py
@@ -3,17 +3,18 @@ import xml.etree.ElementTree as ET
 from Qt import QtWidgets, QtGui, QtCore
 
 
-def _getUI(element):
+def _getUI(element, parent):
     if element.tag not in {"Menu", "Action", "Separator"}:
         return None
     if element.tag == "Menu":
-        m = QtWidgets.QMenu(element.attrib.get("label", ""))
+        m = QtWidgets.QMenu(parent)
+        m.setTitle(element.attrib.get("label", "").decode("utf8"))
         return m
     if element.tag == "Action":
-        a = QtWidgets.QAction(element.attrib.get("label", ""))
+        a = QtWidgets.QAction(element.attrib.get("label", "").decode("utf8"), parent)
         return a
     if element.tag == "Separator":
-        s = QtWidgets.QAction()
+        s = QtWidgets.QAction(parent)
         s.setSeparator(True)
         return s
 
@@ -23,18 +24,18 @@ def _modifyMenu(element, menu):
         if child.tag not in {"Menu", "Action", "Separator"}:
             continue
         if child.tag == "Action":
-            action = _getUI(child)
+            action = _getUI(child, menu)
             menu.addAction(action)
         if child.tag == "Separator":
-            action = _getUI(child)
+            action = _getUI(child, menu)
             menu.addAction(action)
         if child.tag == "Menu":
-            subMenu = _getUI(child)
+            subMenu = _getUI(child, menu)
             menu.addMenu(subMenu)
             _modifyMenu(child, subMenu)
 
 
-def createUiFromXml(xmlpath):
+def createUiFromXml(xmlpath, parent):
     try:
         tree = ET.parse(xmlpath)
     except Exception:
@@ -43,6 +44,6 @@ def createUiFromXml(xmlpath):
     if root.tag not in {"Menu", "MenuBar"}:
         # 为menu.xml,意图支持单menu和多menu
         return None
-    menu = _getUI(root)
+    menu = _getUI(root, parent)
     _modifyMenu(root, menu)
     return menu
